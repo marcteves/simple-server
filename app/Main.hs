901c3e070a49968641fde815b772237b31537a07
Marc Teves <gmarc.teves@gmail.com>
2019-09-19 01:11:58 +0800

    Change internal state to an association list

--- app/Main.hs
+++ app/Main.hs
@@ -2,46 +2,45 @@
 import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
-import Data.IORef (readIORef, modifyIORef, newIORef, IORef)
+import Data.IORef (readIORef, writeIORef, modifyIORef, newIORef, IORef)
 import Data.Maybe (fromJust)
+import Data.List
 import Data.Aeson
 import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
-import qualified Data.Map.Strict as Map
 
-data AppRequest = Send T.Text
+data AppRequest = Send T.Text T.Text
                 | Receive T.Text
                 | Hello
                 | Invalid
 
-app :: IORef (Map.Map T.Text Bool) -> Application
+app :: IORef [(T.Text, T.Text)] -> Application
 app mailbox request respond = do
     response <- appResponse mailbox . appRequest $ request
-    reqBody <- getRequestBodyChunk request
-    putStrLn $ "Received request:\n" ++ show reqBody
+    putStrLn . show . queryString $ request
     respond response
 
 appRequest :: Request -> AppRequest
 appRequest request
     | reqMethod == methodGet = Hello
-    | reqMethod == methodPost = Send address
-    | reqMethod == methodDelete = Receive address
+    | reqMethod == methodPost = Send (qlook "addr") (qlook "message")
+    | reqMethod == methodDelete = Receive (qlook "addr")
     | otherwise = Invalid
-    where address =
-            TE.decodeUtf8 . fromJust . fromJust . lookup "addr" . queryString $ request
+    where qlook str =
+            TE.decodeUtf8 . fromJust . fromJust . lookup str . queryString $ request
           reqMethod = requestMethod request
 
-appResponse :: IORef (Map.Map T.Text Bool) -> AppRequest -> IO Response
-appResponse mailbox (Send addr) = do
-    modifyIORef mailbox $ Map.insert addr True
+appResponse :: IORef [(T.Text, T.Text)] -> AppRequest -> IO Response
+appResponse mailbox (Send addr message) = do
+    modifyIORef mailbox $ (:) (addr, message)
     mails <- readIORef mailbox
     return $ craftResponse mails
 
 appResponse mailbox (Receive addr) = do
-    modifyIORef mailbox $ Map.delete addr
-    mails <- readIORef mailbox
-    return $ craftResponse mails
+    (sent, rest) <- ( partition ((==) addr . fst) ) `liftM` readIORef mailbox
+    writeIORef mailbox rest
+    return $ craftResponse sent
 
 appResponse _ Hello = return (
     responseLBS status200 [(hContentType, "text/html")] "Hello"
@@ -50,13 +49,13 @@ appResponse _ Invalid = return (
     responseLBS status400 [(hContentType, "text/html")] "Bad Request"
     )
 
-craftResponse :: Map.Map T.Text Bool -> Response
+craftResponse :: [(T.Text, T.Text)] -> Response
 craftResponse mails =
     responseLBS status200 [(hContentType, "application/json")] jsonmails
-    where jsonmails = encode . Map.toAscList $ mails
+    where jsonmails = encode mails
 
 main :: IO ()
 main = do
-    mailbox <- newIORef Map.empty :: IO (IORef (Map.Map T.Text Bool))
+    mailbox <- newIORef [] :: IO (IORef ([(T.Text, T.Text)]))
     putStrLn $ "http://localhost:8080/"
     run 8080 $ app mailbox
