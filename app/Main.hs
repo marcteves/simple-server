6c176ab51a5b2ad45f4b8da2a2c5a8e3cdabe861
Marc Teves <gmarc.teves@gmail.com>
2019-09-19 02:31:09 +0800

    Add type synonyms and comments

--- app/Main.hs
+++ app/Main.hs
@@ -11,17 +11,23 @@ import Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 
+-- These data types indicate the requests the web app can process
 data AppRequest = Send T.Text T.Text
                 | Receive T.Text
                 | Hello
                 | Invalid
 
-app :: IORef [(T.Text, T.Text)] -> Application
+type Mail = (T.Text, T.Text)
+type Mailbox = [Mail]
+
+-- Augmented Application type, adding an IORef for tracking internal state
+app :: IORef Mailbox -> Application
 app mailbox request respond = do
     response <- appResponse mailbox . appRequest $ request
     putStrLn . show . queryString $ request
     respond response
 
+-- Take an HTTP request and convert it into our app's self-defined types
 appRequest :: Request -> AppRequest
 appRequest request
     | reqMethod == methodGet = Hello
@@ -32,7 +38,7 @@ appRequest request
             TE.decodeUtf8 . fromJust . fromJust . lookup str . queryString $ request
           reqMethod = requestMethod request
 
-appResponse :: IORef [(T.Text, T.Text)] -> AppRequest -> IO Response
+appResponse :: IORef Mailbox -> AppRequest -> IO Response
 appResponse mailbox (Send addr message) = do
     modifyIORef mailbox $ (:) (addr, message)
     mails <- readIORef mailbox
@@ -50,14 +56,15 @@ appResponse _ Invalid = return (
     responseLBS status400 [(hContentType, "text/html")] "Bad Request"
     )
 
-craftResponse :: [(T.Text, T.Text)] -> Response
+craftResponse :: Mailbox -> Response
 craftResponse mails =
     responseLBS status200 [(hContentType, "application/json")] jsonmails
     where jsonmails = encode mails
 
+-- main just calls the augmented Application above with an IORef to the 
+-- internal state and a port number
 main :: IO ()
 main = do
     args <- getArgs
-    mailbox <- newIORef [] :: IO (IORef ([(T.Text, T.Text)]))
-    putStrLn $ "http://localhost:8080/"
+    mailbox <- newIORef [] :: IO (IORef Mailbox)
     run (read . head $ args :: Int) $ app mailbox
