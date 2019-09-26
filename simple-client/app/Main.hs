2cdebb1a865e6d339e64028ff3f5ddf969e7d012
Marc Teves <gmarc.teves@gmail.com>
2019-09-26 14:12:34 +0800

    Bring simple-client to working state

--- simple-client/app/Main.hs
+++ simple-client/app/Main.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import SimpleServer.Types
+
+import Data.IORef (readIORef, writeIORef, modifyIORef, newIORef, IORef)
+import Network.HTTP.Simple
+import Control.Applicative
+import Control.Concurrent
+import Control.Monad
+import System.Environment
+import qualified Data.Text as T
+import Data.Aeson
 
 main :: IO ()
-main = someFunc
+main = do
+    [addr, url] <- getArgs
+    let newURL  = url ++ "?addr=" ++ addr
+    let request = setRequestMethod "DELETE" . parseRequest_ $ newURL
+    let address = T.pack addr
+    state      <- newIORef $ Mail address "default" :: IO (IORef Mail)
+    forever $ pollLoop state request
+
+pollLoop state request = do
+        response <- (eitherDecode . getResponseBody) `liftM` httpLBS request
+        case response of
+            Left err ->         putStrLn err
+            Right mails
+                | null mails -> return ()
+                | otherwise  -> writeIORef state (last mails)
+        lastMail <- readIORef state
+        let print = T.unpack $ "Last message received: " `T.append` (message lastMail)
+        putStrLn print
+        threadDelay 1000000
